2c03048ffe186e6be0f80ae9cf9814776aafb2b4
hadley <h.wickham@gmail.com>
2015-12-21 13:28:27 -0600

    First pass on website

--- js/freelancer.js
+++ js/freelancer.js
@@ -15,17 +15,6 @@ $(function() {
     });
 });
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
-
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://hadley.nz --region us-west-1
+aws s3 sync . s3://hadley.nz --region us-west-1 --delete
